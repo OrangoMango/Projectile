63b8dbb07df549e0a26813b4a5913d539781835f
OrangoMango <orapama06@gmail.com>
2022-04-26 17:30:48 +0200

    Changed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd src
 echo "compiling source code.."
-javac -cp .:../lib/json.jar -d ../bin --module-path $FX_PATH --add-modules javafx.controls,javafx.media com/orangomango/projectile/*.java -Xlint:deprecation
+javac -cp .:../lib/json.jar -d ../bin --module-path $1 --add-modules javafx.controls,javafx.media com/orangomango/projectile/*.java -Xlint:deprecation
 cd ../bin
 echo "executing binary files.."
-java -cp .:../lib/json.jar --module-path $FX_PATH --add-modules javafx.controls,javafx.media com.orangomango.projectile.MainApplication
+java -cp .:../lib/json.jar --module-path $1 --add-modules javafx.controls,javafx.media com.orangomango.projectile.MainApplication
